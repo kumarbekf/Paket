b375a084118b30a174e340ff559e7521a5619f4d
kumarbek <kumarbektolonbayev@gmail.com>
2023-07-19 12:35:56 +0300

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    private double rabius;
+    static final double PI = 3.14;
+
+   public static double Area(double rabius2) {
+    return PI * (rabius2 * rabius2);
+    }
+   public static double circumference(double rabius3) {
+   return PI * 2 * rabius3;
+    }
+
+    }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Circle.Area(330));
+        System.out.println(Circle.circumference(55000.0));
+
+
+    }
+}
